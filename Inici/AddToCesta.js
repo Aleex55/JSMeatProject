9059578061dff30c257556a491403df0e896ff37
Aleex55 <94048476+Aleex55@users.noreply.github.com>
2022-01-12 11:02:08 +0100

    ahora la cantidad no se modificara desde el producto sino de cesta

--- Inici/AddToCesta.js
+++ Inici/AddToCesta.js
@@ -7,7 +7,6 @@ addToCart.forEach(addToCart => {
 const shoppingCart = document.querySelector('.shoppingCart');
 let nomProds = [];
 let priceProds = [];
-let quantProds = [];
 let imgProds = [];
 
 function addedToCart(event) { 
@@ -15,22 +14,19 @@ function addedToCart(event) {
     const item = button.closest('.item');
     const itemTitle = item.querySelector('.item-title').textContent;
     const itemPrice = item.querySelector('.item-price').textContent;
-    const itemQuantity = item.querySelector('.item-quantity').value;
     const itemImage = item.querySelector('.item-image').src;
 
-    saveItem(itemTitle, itemPrice, itemQuantity, itemImage);
+    saveItem(itemTitle, itemPrice, itemImage);
 }
 
 
-function saveItem(itemTitle, itemPrice, itemQuantity, itemImage){
+function saveItem(itemTitle, itemPrice, itemImage){
     nomProds.push(itemTitle);
     priceProds.push(itemPrice);
-    quantProds.push(itemQuantity);
     imgProds.push(itemImage);
 
     sessionStorage.setItem('arrayNomProducts', JSON.stringify(nomProds));
     sessionStorage.setItem('arrayPriceProducts', JSON.stringify(priceProds));
-    sessionStorage.setItem('arrayQuantProducts', JSON.stringify(quantProds));
     sessionStorage.setItem('arrayImgProducts', JSON.stringify(imgProds));
 
     console.log("Item afegit al carro")
