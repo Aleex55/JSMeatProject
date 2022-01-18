796b3c50e1415ab833436eff1c095a15b3e6c2c9
Aleex55 <94048476+Aleex55@users.noreply.github.com>
2022-01-18 09:57:22 +0100

    fix: ahora se añaden los items correctamente

--- Inici/AddToCesta.js
+++ Inici/AddToCesta.js
@@ -1,20 +1,64 @@
-const addToCart = document.querySelectorAll('.compra'); //classe boton compra
+let itemToAdd = document.querySelectorAll('.compra'); //classe boton compra
 
-addToCart.forEach(addToCart => {
-    addToCart.addEventListener('click', addedToCart);
-});
-
-const shoppingCart = document.querySelector('.shoppingCart');
 let nomProds = [];
-let priceProds = [];
+let priceProds = []; 
 let imgProds = [];
 
+
+if (localStorage.getItem("arrayNomProducts") !== null) {
+    nomProds = JSON.parse(localStorage.getItem('arrayNomProducts'));
+    priceProds = JSON.parse(localStorage.getItem('arrayPriceProducts'));
+    imgProds = JSON.parse(localStorage.getItem('arrayImgProducts'));
+}
+
+
+
+itemToAdd.forEach(addToCartButton => {
+    addToCartButton.addEventListener('click', addedToCart);
+})
+
+function addedToCart() { 
+
+    let itemTitle = document.querySelector('.item-title').textContent;
+    let itemPrice = document.querySelector('.item-price').textContent;
+    let itemImage = document.querySelector('.item-image').src;
+
+    let titulo = String(itemTitle);
+    let precio = String(itemPrice);
+    let imagen = String(itemImage);
+
+    console.log(titulo, precio, imagen);
+
+    nomProds.push(titulo);
+    priceProds.push(precio);
+    imgProds.push(imagen);
+    
+    localStorage.setItem('arrayNomProducts', JSON.stringify(nomProds));
+    localStorage.setItem('arrayPriceProducts', JSON.stringify(priceProds));
+    localStorage.setItem('arrayImgProducts', JSON.stringify(imgProds));
+    console.log("Item afegit al carro");
+}
+
+
+/*
+const shoppingCart = document.querySelector('.shoppingCart');
+let nomProds = [];
+let priceProds = []; 
+let imgProds = [];
+
+nomProds = JSON.parse(localStorage.getItem('arrayNomProducts'));
+priceProds = JSON.parse(localStorage.getItem('arrayPriceProducts'));
+imgProds = JSON.parse(localStorage.getItem('arrayImgProducts'));
+
+
+
 function addedToCart(event) { 
-    const button = event.target;
-    const item = button.closest('.item');
-    const itemTitle = item.querySelector('.item-title').textContent;
-    const itemPrice = item.querySelector('.item-price').textContent;
-    const itemImage = item.querySelector('.item-image').src;
+    let button = event.target;
+    let item = button.closest('.item');
+    let itemTitle = item.querySelector('.item-title').textContent;
+    let itemPrice = item.querySelector('.item-price').textContent;
+    let itemImage = item.querySelector('.item-miniimage').src;
+
 
     saveItem(itemTitle, itemPrice, itemImage);
 }
@@ -25,9 +69,20 @@ function saveItem(itemTitle, itemPrice, itemImage){
     priceProds.push(itemPrice);
     imgProds.push(itemImage);
 
-    sessionStorage.setItem('arrayNomProducts', JSON.stringify(nomProds));
-    sessionStorage.setItem('arrayPriceProducts', JSON.stringify(priceProds));
-    sessionStorage.setItem('arrayImgProducts', JSON.stringify(imgProds));
-
-    console.log("Item afegit al carro")
+    saveArrays();
+    for (let i = 0; i < nomProds.length; i++) {
+        let nombre = nomProds[i];
+        let precio = priceProds[i];
+        let imagen = imgProds[i];
+        console.log(nombre , precio, imagen);
+    }
 }
+
+function saveArrays () {
+    localStorage.setItem('arrayNomProducts', JSON.stringify(nomProds));
+    localStorage.setItem('arrayPriceProducts', JSON.stringify(priceProds));
+    localStorage.setItem('arrayImgProducts', JSON.stringify(imgProds));
+    console.log("Item afegit al carro");
+    
+}
+*/
