0b8f89b6a4570b885461e0dc471e8383dc9e02dd
Aleex55 <94048476+Aleex55@users.noreply.github.com>
2022-01-19 13:30:57 +0100

    items add to cesta.html

--- Inici/PutOnCesta.js
+++ Inici/PutOnCesta.js
@@ -2,67 +2,47 @@ let itemTitles = [];
 let itemPrices = [];
 let itemImages = [];
 
-const allCartItems = document.querySelector('.allcart-items');
+
+const allCartItemsContainer = document.querySelector('.allcart-items');
 
 itemTitles = JSON.parse(localStorage.getItem('arrayNomProducts'));
 itemPrices = JSON.parse(localStorage.getItem('arrayPriceProducts'));
 itemImages = JSON.parse(localStorage.getItem('arrayImgProducts'));
 
-function addItemTOCart(itemTitle, itemPrice, itemImage){
-    const row = document.createElement('div');
-    const rowContent = `
-    <div class="cart-item">
-        <tr>
-            <td>
-                <img class="item-minimage" src=${itemImage}>
-            </td>
-            <td>
-                <p class="item-name">${itemTitle}</p>
-            </td>
-            <td>
-                <input class="item-quantity" type="number" value="1"></input>
-            </td>
-            <td>
-                <p class="item-price">${itemPrice}</p>
-            </td>
-            <td>
-                <button class="item-elim" type="button">X</button>
-            </td>
-        </tr>
-    <div>`;
-    row.innerHTML = rowContent;
-    allCartItems.append(row);
 
-    row.querySelector('.eliminate').addEventListener('click', itemRemover)
-
-    updateTotalPrice();
+if (localStorage.getItem("arrayNomProducts") !== null) {
+addItemsToCart(itemTitles, itemPrices, itemImages);
 }
 
-function updateTotalPrice(){
-    let total = 0;
+function addItemsToCart(itemTitles, itemPrices, itemImages){
+    console.log(itemTitles, itemPrices, itemImages);
 
-    const totalCartPrice = document.querySelector('.total-price');
+    for (let i = 0; i < itemTitles.length; i++) {
+       const cartRow = document.createElement('div');
+        const rowContent = `
+        <div class="table-row cart-item">
+                    <div class = "table-cell">
+                        <img class="item-minimage" src="${itemImages[i]}">
+                    </div>
+                    <div class = "table-cell">
+                        <p class="item-name">${itemTitles[i]}</p>
+                    </div>
+                    <div class = "table-cell">
+                        <input class="item-quantity" type="number" value="1"></input>
+                    </div>
+                    <div class = "table-cell">
+                        <p class="item-price">${itemPrices[i]}</p>
+                    </div>
+                    <div class = "table-cell">
+                        <button class="eliminate-button" type="button">X</button>
+                    </div>
+            </div>`;
+        
+        allCartItemsContainer.append(cartRow);
+        cartRow.innerHTML = rowContent;
+        
+    }
 
-    const allItems = document.querySelectorAll('.cart-item');
+    
 
-    allItems.forEach(cartItem => {
-        const cartItemPriceElement = cartItem.querySelector('.item-price');
-
-        const cartItemPrice = Number(cartItemPriceElement.textContent.replace('€',''));
-
-
-        const cartItemQuantityElement = cartItem.querySelector('.item-quantity');
-
-        const cartItemQuantity = Number(cartItemQuantityElement.value);
-
-        total = total + (cartItemPrice * cartItemQuantity);
-    })
-
-    totalCartPrice.innerHTML = `${total.toFixed(2)}€`;
-}
-
-function itemRemover(){
-    const buttonClicked = event.target;
-    buttonClicked.closest('.cart-item').remove();
-    updateTotalPrice();
-}
+};
